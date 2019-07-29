c11732ea19587cf71ffab49dbc6679b81d8aeeee
owenleej <owenleej@gmail.com>
2019-07-29 12:01:28 -0600

    changing to http-server for webserver

--- logTest.js
+++ logTest.js
@@ -1,3 +1,3 @@
 module.exports = async function(context, commands) {
-    return await commands.measure.start('http://localhost:8000/baseline.html', 'baseline');
+    return await commands.measure.start('http://localhost:8080/baseline.html', 'baseline');
 };
